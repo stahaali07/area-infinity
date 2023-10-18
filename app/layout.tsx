61ff50165ad172a249f3c4dcb3e0ed13a6757191
stahaali07 <staha.ali07@gmail.com>
2023-10-18 22:51:35 +0500

    Navbar UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
 
-const nunito = Nunito({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Area Infinity',
-  description: 'Bridging Web3 with Real Life Architecture',
-}
+  title: "Area Infinity",
+  description: "Bridging Web3 with Real Life Architecture",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
